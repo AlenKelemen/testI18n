d03db6ba9a05f22176f7d8d54a248a70a2e6eca4
Alen <alen@edc.hr>
2026-03-29 20:32:10 +0200

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import i18n, { t } from './i18n.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 document.body.innerHTML = `
+<div class="container-fluid">
   <h1 id="title"></h1>
-  <button id="hr">HR</button>
-  <button id="en">EN</button>
+  <button id="hr" class="btn btn-primary">HR</button>
+  <button id="en" class="btn btn-primary">EN</button>
   <p id="zoomIn"></p>
+</div>
 `;
 function render() {
   document.getElementById('title').textContent = t('app.title');
